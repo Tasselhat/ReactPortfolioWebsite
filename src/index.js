9e8e2c698bad1ba4d2b13780db092905e67f27ad
Tim <tim.b.schneider@gmail.com>
2022-09-11 12:19:37 -0700

    scroll on routing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./Hooks/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename="/ReactPortfolioWebsite">
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );

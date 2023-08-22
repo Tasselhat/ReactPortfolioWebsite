0366575e43c7ebf1559395987db97c20b80e213e
Tim Schneider <TIM.B.SCHNEIDER@GMAIL.COM>
2023-08-22 00:37:52 -0700

    fonts

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -5,31 +5,32 @@ import Console from "./Console";
 import "../index.css";
 import "./Terminal.css";
 
-import "../Fonts/WebPlus_IBM_VGA_8x16-2x.woff";
-import "../Fonts/WebPlus_IBM_VGA_8x16.woff";
-import "../Fonts/Px437_IBM_VGA_8x16.ttf";
+import "../../public/Fonts/WebPlus_IBM_VGA_8x16-2x.woff2";
+import "../../public/Fonts/WebPlus_IBM_VGA_8x16.woff";
+import "../../public/Fonts/Px437_IBM_VGA_8x16-2x.ttf";
+import "../../public/Fonts/Px437_IBM_VGA_8x16.ttf";
 
 export default class About extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {};
-  }
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <main id="page" className="terminal-container" role="main">
-        <div className="tv">
-          <Console />
-          <div className="collection">
-            More: <Link to="/Projects">Projects</Link>, or
-            <Link to="/Resume"> Resume</Link> and
-            <Link to="/Contact"> Contact</Link>
-          </div>
-          <div className="flicker"></div>
-          <div className="scanlines"></div>
-        </div>
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main id="page" className="terminal-container" role="main">
+				<div className="tv">
+					<Console />
+					<div className="collection">
+						More: <Link to="/Projects">Projects</Link>, or
+						<Link to="/Resume"> Resume</Link> and
+						<Link to="/Contact"> Contact</Link>
+					</div>
+					<div className="flicker"></div>
+					<div className="scanlines"></div>
+				</div>
+			</main>
+		);
+	}
 }

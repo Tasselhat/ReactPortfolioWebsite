cdb4f54dcc909d11bfd96ddff652435863026aac
Tim Schneider <TIM.B.SCHNEIDER@GMAIL.COM>
2023-08-22 00:40:11 -0700

    fonts

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -5,10 +5,9 @@ import Console from "./Console";
 import "../index.css";
 import "./Terminal.css";
 
-import "../../public/Fonts/WebPlus_IBM_VGA_8x16-2x.woff2";
-import "../../public/Fonts/WebPlus_IBM_VGA_8x16.woff";
-import "../../public/Fonts/Px437_IBM_VGA_8x16-2x.ttf";
-import "../../public/Fonts/Px437_IBM_VGA_8x16.ttf";
+import "../Fonts/WebPlus_IBM_VGA_8x16.woff";
+import "../Fonts/Px437_IBM_VGA_8x16-2x.ttf";
+import "../Fonts/Px437_IBM_VGA_8x16.ttf";
 
 export default class About extends React.Component {
 	constructor(props) {
